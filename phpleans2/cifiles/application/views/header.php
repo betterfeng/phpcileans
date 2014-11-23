77bcf3ea1f7feedc9882bf5f90496198590cbb76
betterwin7 <betterwin7@betterwin7-PC>
2014-11-23 14:26:49 +0800

    css style

---
+++ phpleans2/cifiles/application/views/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>这是头部</title>
+        <link rel='stylesheet' type="text/css" href="uploads/mainbutton.css" />
+        <style type="text/css">
+             .bodybg2 {
+                 background-image:url(uploads/1.png);
+                 background-repeat:no-repeat;
+                 width: 100px;
+                 height: 21px;
+             }
+            
+        </style>
+    </head>
+ 
---
+++ phpleans2/cifiles/application/views/mainbody.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+
+<body class="bodybg">
+    <input  class="ok3" type="submit" name="login" value="登录" /><br/><br/>
+    <input  type="submit" name="login" value="提 交" style="background-color: #fff; background-image: url(../uploads/1.png);background-position: -1px -94px;cursor: pointer;font-size: 18px;width: 252px;height: 58px;border:   0;" />
+</body>
+
